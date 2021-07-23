2ea1fb213c1189711179a541ea5788ae3998c90b
Deniz <denizkerim@yahoo.com>
2021-07-23 04:25:35 -0500

    create contact form, style contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,9 +2,31 @@ import React from "react";
 
 import './contact.scss';
 
-const Contact = () =>
-  <section id='contact'>
-    <h2>Contact</h2>
-  </section>
+const Contact = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  }
+
+  return (
+    <section id='contact'>
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Your Name</p>
+          <input type='text' name='name' placeholder='John Doe' />
+        </label>
+        <label>
+          <p>Your Email</p>
+          <input type='text' name='email' placeholder='doejohn@email.com' />
+        </label>
+        <label>
+          <p>Your Message</p>
+          <textarea name='message' placeholder={`You're the coolest candidate I've ever seen because...`} />
+        </label>
+        <input type='submit' value='Send' />
+      </form>
+    </section>
+  );
+}
 
 export default Contact
