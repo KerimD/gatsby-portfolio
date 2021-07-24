79c20315bac40b10212ae19bc3e94ed399cce88b
Deniz <denizkerim@yahoo.com>
2021-07-24 09:01:50 -0500

    projects are now two columns

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 
-// import Project from './Project';
-
 import './projects.scss';
 import { PROJECTS } from '../../assets/data';
-import Github from '../../assets/images/svg/github.svg';
+
+import Project from "./Project";
+
+const PROJECTS_LEFT: TypesN.Project[] = [];
+const PROJECTS_RIGHT: TypesN.Project[] = [];
+
+PROJECTS.forEach((e, i) => i % 2 == 0 ? PROJECTS_RIGHT.push(e) : PROJECTS_LEFT.push(e));
 
 const Projects = () =>
-  <section id="projects">
+  <section id='projects'>
     <h2>Projects</h2>
-    <div className="projects-container">
-      {PROJECTS.map((project) =>
-        <div className='project' key={project.name} >
-          {project.title != 'Solo Developer'
-            ? <h3>{project.title}, {project.name}</h3>
-            : <h3>{project.name}</h3>}
-          <div className='project-date'>{project.date}</div>
-          {project.description}
-          <a href={project.github} target="_blank">
-            <Github height='22' />
-          </a>
-          <a className='image-link' href={project.liveSite} target="_blank">
-            <img src={project.imageSrc} alt={project.name + ' image'} />
-          </a>
-        </div>
-      )}
+    <div className='projects-container'>
+      <div className='projects-left'>
+        {PROJECTS_LEFT.map((project) =>
+          <Project project={project} />
+        )}
+      </div>
+      <div className='projects-right'>
+        {PROJECTS_RIGHT.map((project) =>
+          <Project project={project} />
+        )}
+      </div>
     </div>
   </section>
 
