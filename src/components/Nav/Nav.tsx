f59f12ddde3deae50a7da9d5e4deaf42bdad0def
Deniz <denizkerim@yahoo.com>
2021-08-05 17:23:24 -0500

    undo nav refactor cuz idk why it won't work sometimes

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import './nav.css';
 
-const sections = Array.from(document.getElementsByTagName('section'));
-
 const Nav = () =>
   <nav className='nav'>
     <a href='#' className='nav-logo'>Deniz Kerim</a>
     <ul className='nav-link-list'>
-      {sections.map((element) =>
-        <li><a href={`#${element.id}`}>{element.id}</a></li>
-      )}
+      <li><a href='#projects'>Projects</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#contact'>Contact</a></li>
     </ul>
   </nav>
 
