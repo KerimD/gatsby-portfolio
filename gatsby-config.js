08e2ebf87b79d68f6fe83a982e9e622b0ed68113
Deniz <denizkerim@yahoo.com>
2022-02-04 23:10:21 -0600

    add and configure contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
     {
