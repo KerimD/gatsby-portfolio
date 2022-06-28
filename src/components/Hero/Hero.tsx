402b48cdc7332a0c96315e6f28a82c48737deb81
Deniz <denizkerim@yahoo.com>
2022-06-28 16:51:50 +0300

    change minor styling, change hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,15 @@ const Hero = ({ links }: TypesN.Links) =>
       <h1>Deniz Kerim</h1>
       <p>
         Welcome!<br />
-        {`I'm a Computer Science student at Missouri University of Science & Technology`}
+        {"I'm a fresh graduate from "}
+        <a
+          href='https://www.mst.edu/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          {"Missouri University of Science & Technology"}
+        </a>
+        {" with a B.S. in Computer Science!"}
       </p>
     </div>
     <HeroLinks links={links} />
